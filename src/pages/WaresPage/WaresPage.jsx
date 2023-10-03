ab8b54f3e4a98b52ddad46bbb105048a6c0058d1
Robbie Gollan <robbiegollan@gmail.com>
2023-10-03 23:31:11 +1100

    adds wares page with navbar and featured items

--- src/pages/WaresPage/WaresPage.jsx
+++ src/pages/WaresPage/WaresPage.jsx
@@ -1,5 +1,14 @@
+import styles from "./WaresPage.module.scss";
+import Navbar from "../../components/Navbar/Navbar";
+import FeaturedItems from "../../components/FeaturedItems/FeaturedItems";
+
 const WaresPage = () => {
-	return <div>WaresPage</div>;
+	return (
+		<div className={styles.wares_page}>
+			<Navbar />
+			<FeaturedItems />
+		</div>
+	);
 };
 
 export default WaresPage;
