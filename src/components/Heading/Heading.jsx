a6f746ae6716bb73a0677751ecabac4b6b35091d
Robbie Gollan <robbiegollan@gmail.com>
2023-10-05 18:02:08 +1100

    adds some ware page content, gallery page

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,12 +1,12 @@
 import styles from "./Heading.module.scss";
 
-const Heading = ({ text, size = "36px", truncation = 28 }) => {
+const Heading = ({ text, size = "24px", truncation = 40 }) => {
 	if (text.length > truncation) {
 		text = text.substring(0, truncation) + "...";
 	}
 
 	return (
-		<h2 className={styles.heading} style={{ fontSize: { size } }}>
+		<h2 style={{ fontSize: `${size}` }} className={styles.heading}>
 			{text}
 		</h2>
 	);
--- src/pages/WarePage/WarePage.jsx
+++ src/pages/WarePage/WarePage.jsx
@@ -1,9 +1,61 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getItemById } from "../../services/fake-backend-services";
+import styles from "./WarePage.module.scss";
+import Navbar from "../../components/Navbar/Navbar";
+import Heading from "../../components/Heading/Heading";
+import Gallery from "../../components/Gallery/Gallery";
 
 const WarePage = () => {
-	const { slug } = useParams();
+	const { id } = useParams();
+	const [wareData, setWareData] = useState(null);
 
-	return <div>Product page for {slug}</div>;
+	useEffect(() => {
+		const fetchData = async (itemID) => {
+			try {
+				const data = await getItemById(itemID);
+				console.log(data);
+				setWareData(data);
+			} catch (e) {
+				console.error(e.message);
+			}
+		};
+		fetchData(id);
+	}, [id]);
+
+	if (!wareData) {
+		return (
+			<div className={styles.ware_page}>
+				<Navbar />
+				<p>Loading...</p>
+			</div>
+		);
+	}
+
+	const {
+		// categories,
+		// description,
+		// developer,
+		// discountFactor,
+		// featured,
+		imageLinks,
+		// price,
+		// publisher,
+		// rating,
+		// reviewScore,
+		// stock,
+		title,
+	} = wareData;
+
+	return (
+		<div className={styles.ware_page}>
+			<Navbar />
+			<main className={styles.ware_page__container}>
+				<Heading text={title} size='60px' />
+				<Gallery imageLinks={imageLinks} />
+			</main>
+		</div>
+	);
 };
 
 export default WarePage;
